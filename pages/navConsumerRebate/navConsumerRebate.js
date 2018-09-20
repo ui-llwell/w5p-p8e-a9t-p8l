d07daa0e920cfd5331760d0a027087b0982ea483
jinglin-llwell <jinglin@llwell.net>
2018-09-20 17:52:37 +0800

    注册业务员、收款业务、及详情
    closes #23, closes #24, closes #25

--- pages/navConsumerRebate/navConsumerRebate.js
+++ pages/navConsumerRebate/navConsumerRebate.js
@@ -1,66 +1,38 @@
-// pages/navConsumerRebate/navConsumerRebate.js
+// // pages/navConsumerRebate/navConsumerRebate.js
+
+//获取应用实例
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
+  //事件处理函数
   onShareAppMessage: function () {
-
-  }
+    return {
+      title: 'XXXXXXXXXX',
+      imageUrl: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/PurchasingAssistantPersonal/shop_icon_phone@3x.png',
+      path: '/page/user?id=123',
+      success: function (res) {
+        var shareTickets = res.shareTickets;
+        if (shareTickets.length == 0) {
+          return false;
+        }
+        wx.getShareInfo({
+          shareTicket: shareTickets[0],
+          success: function (res) {
+            var encryptedData = res.encryptedData;
+            var iv = res.iv;
+          }
+        })
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+  onLoad: function () {
+    wx.showShareMenu({
+      withShareTicket: true
+    })
+  },
 })
