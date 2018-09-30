73cbf87976aa8861a671d9006584dbc7df5e575c
Una <una_bella@qq.com>
2018-09-30 15:30:11 +0800

    【细节处理】
    【细节处理】

--- pages/start/start.js
+++ pages/start/start.js
@@ -16,7 +16,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // app.login(options)
+    app.login(options)
     // console.log('~',options)
     // console.log('userType', wx.getStorageSync('userType'))
     
